32a2e4d2774a1cdbb216dd2603fc9c66af1b3d44
jin1125 <shogo.jinta@gmail.com>
2021-11-18 17:45:40 +0900

    25

--- work/web/index.php
+++ work/web/index.php
@@ -6,23 +6,9 @@ include('../app/_parts/_header.php');
 
 ?>
 
-<form action="result.php" method='get'>
-  <label>
-    <input type="radio" name="color" value='orange'>
-    orange
-  </label>
+<form action="result.php" method='post'>
 
-  <label>
-    <input type="radio" name="color" value='pink'>
-    pink
-  </label>
-
-  <label>
-    <input type="radio" name="color" value='gold'>
-    gold
-  </label>
   <button>Send</button>
-  <a href="reset.php">[reset]</a>
 </form>
 
 <?php
