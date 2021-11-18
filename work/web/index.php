2a2b665f5ef928a806c9b4ea836275badb6e93e3
jin1125 <shogo.jinta@gmail.com>
2021-11-18 15:25:02 +0900

    15

--- work/web/index.php
+++ work/web/index.php
@@ -7,11 +7,20 @@ include('../app/_parts/_header.php');
 ?>
 
 <form action="result.php" method='get'>
-  <select name="color">
-    <option value="orange">orange</option>
-    <option value="pink">pink</option>
-    <option value="gold">gold</option>
-  </select>
+  <label>
+    <input type="checkbox" name="colors[]" value='orange'>
+    orange
+  </label>
+
+  <label>
+    <input type="checkbox" name="colors[]" value='pink'>
+    pink
+  </label>
+
+  <label>
+    <input type="checkbox" name="colors[]" value='gold'>
+    gold
+  </label>
   <button>Send</button>
 </form>
 
