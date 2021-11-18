70757aeb9aac1b236b0a3390c248b3faf50ccf50
jin1125 <shogo.jinta@gmail.com>
2021-11-18 21:05:37 +0900

    33

--- work/app/functions.php
+++ work/app/functions.php
@@ -4,3 +4,17 @@ function h($str)
 {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
+
+function createToken(){
+  if(!isset($SESSION['token'])){
+    $SESSION['token'] = bin2hex(random_bytes(32));
+  }
+}
+
+function validateToken(){
+  if(empty($_SESSION['token']) || $_SESSION['token'] !== filter_input(INPUT_POST,'token')){
+    exit('Invalid post request');
+  }
+}
+
+session_start();
